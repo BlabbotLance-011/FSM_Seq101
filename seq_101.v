f0e02347f1003f7f53a8cd18b4d3d29f5386189b
BlabbotLance-011 <lancesalanap@gmail.com>
2026-04-05 21:36:34 +0800

    Update seq_101.v

--- seq_101.v
+++ seq_101.v
@@ -1,12 +1,14 @@
 /*====================================
-				SEQUENCE 101
+								SEQUENCE 101
 ======================================
 DESCRIPTION:
 
+
 DESIGN ENGINEER:
+Salanap, Lance Kelly D.
 
 DATE:
-
+5 Apr 2026
 
 ------------------------------------*/
 module seq_101(out, state, clk, rst_n, in);
@@ -19,12 +21,13 @@ module seq_101(out, state, clk, rst_n, in);
 	
 	
 	//state
-	parameter [1:0]S0 = 2'b00;
-	parameter [1:0]S1 = 2'b01;
-	parameter [1:0]S2 = 2'b10;
-	parameter [1:0]S3 = 2'b11;
-	reg [1:0]pre; //present state
-	reg [1:0]nxt; //next state
+	parameter [1:0] S0 = 2'b00;
+	parameter [1:0] S1 = 2'b01;
+	parameter [1:0] S2 = 2'b10;
+	parameter [1:0] S3 = 2'b11;
+	reg [1:0] pres; //present state
+	reg [1:0] next; //next state
+
 	/*
 	//1Hz Clock
 		clk_div #(.TICKS_500MS(25_000_000)) div(
@@ -35,45 +38,48 @@ module seq_101(out, state, clk, rst_n, in);
 	//this convert the 50MHz clock to 1Hz Clock
  
 	*/
+
 	//input block
-	always @(in,pre) begin
-		case(pre)
-			S0:nxt = in? S1:S0;
-			S1:nxt = in? S1:S2;
-			S2:nxt = in? S3:S0;
-			S3:nxt = in? S1:S0;
-			default:nxt = S0;
+	always @(in,pres) begin
+		case(pres)
+			S0 : next = in ? S1 : S0;
+			S1 : next = in ? S1 : S2;
+			S2 : next = in ? S3 : S0;
+			S3 : next = in ? S1 : S0;
+			default: next = S0;
 		endcase //end of case
 	end //end of always @(in,pre)
 	
-	//sequential block
+	//Sequential Block
 	always @(posedge clk, negedge rst_n) begin
-		if(!rst_n) pre = S0;
-		else pre = nxt;
+		if(!rst_n) pres = S0;
+		else pres = next;
+
 	end //end of always@(posedge clk, negedge rst_n)
-	//output block
-	always@(pre)begin
-		case (pre)
-			S0:begin	
+
+	//Output Block
+	always@(pres)begin
+		case (pres)
+			S0: begin	
 					out = 0;
 					state = S0;
-				end //end of S0
-			S1:begin	
+				 end //end of S0
+			S1: begin	
 					out = 0;
 					state = S1;
-				end //end of S1
-			S2:begin	
+				 end //end of S1
+			S2: begin	
 					out = 0;
 					state = S2;
-				end //end of S2
-			S3:begin	
+				 end //end of S2
+			S3: begin	
 					out = 1;
 					state = S3;
-				end //end of S3
-			default:begin	
+				 end //end of S3
+			default: begin	
 					out = 0;
 					state = S0;
-				end //end of default
+					end //end of default
 		endcase //end of case
 	 end //end of always@(pre)
 endmodule
